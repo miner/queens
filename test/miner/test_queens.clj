b7b4b4839cb0dd02a52d35360ae59fbeeb56925c
Steve Miner <steveminer@gmail.com>
2018-11-19 10:43:57 -0500

    clean up

--- test/miner/test_queens.clj
+++ test/miner/test_queens.clj
@@ -2,3 +2,10 @@
   (:require [clojure.test :refer :all]
             [miner.queens :refer :all]))
 
+
+(deftest eight-queens []
+  (let [dim 8
+        answer-set (set (queens dim))]
+    (doseq [qf [rosetta-queens queens richards-queens]]
+      (is (= answer-set (set (qf dim)))))))
+
