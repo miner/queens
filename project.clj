5ce37b10236a7b2f22b439725943274ef709bdbf
Steve Miner <steveminer@gmail.com>
2020-12-13 10:44:12 -0500

    int-map update

--- project.clj
+++ project.clj
@@ -5,6 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [tarantella "1.1.1"]
-                 [org.clojure/data.int-map "0.2.4"]
+                 [org.clojure/data.int-map "1.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]])
 
