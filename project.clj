047e4b28f45b0e5811cc243c2222ca483466ed43
Steve Miner <steveminer@gmail.com>
2022-01-13 17:31:30 -0500

    libs update

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.velisco/queens "0.2.1"
+(defproject com.velisco/queens "0.2.2"
   :description "Eight Queens in Clojure"
   :url "https://github.com/miner/queens"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [tarantella "1.1.1"]
                  [org.clojure/data.int-map "1.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]])
