32bc9a299c36c90a07454a0554351fff0fc7edd9
Steve Miner <steveminer@gmail.com>
2025-08-26 17:19:20 -0400

    libs

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/miner/queens"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.12.2"]
                  [tarantella "1.1.1"]
-                 [org.clojure/data.int-map "1.0.0"]
-                 [org.clojure/math.combinatorics "0.1.6"]])
+                 [org.clojure/data.int-map "1.3.0"]
+                 [org.clojure/math.combinatorics "0.3.0"]])
 
